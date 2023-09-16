4f85b1700276e8f20b3e6c22774c164e862c60fc
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-09-15 21:12:10 -0400

    models and views update

---
+++ models/tasks.js
@@ -0,0 +1,67 @@
+const tasks = [
+  {
+    "startTime": "2023-09-15T13:00:00Z",
+    "endTime": "2023-09-15T15:00:00Z",
+    "comments": "Lunch break.",
+    
+  },
+  {
+    "startTime": "2023-09-15T10:30:00Z",
+    "endTime": "2023-09-15T11:30:00Z",
+    "comments": "Phone call with client.",
+    
+  },
+  {
+    "startTime": "2023-09-15T14:00:00Z",
+    "endTime": "2023-09-15T16:00:00Z",
+    "comments": "Product demo with engineering team.",
+    
+  },
+  {
+    "startTime": "2023-09-15T09:00:00Z",
+    "endTime": "2023-09-15T10:00:00Z",
+    "comments": "Team stand-up meeting.",
+    
+  },
+  {
+    "startTime": "2023-09-15T11:00:00Z",
+    "endTime": "2023-09-15T12:30:00Z",
+    "comments": "Product brainstorming session.",
+    
+  },
+  {
+    "startTime": "2023-09-15T16:30:00Z",
+    "endTime": "2023-09-15T17:30:00Z",
+    "comments": "Team building event.",
+    
+  },
+  {
+    "startTime": "2023-09-15T10:00:00Z",
+    "endTime": "2023-09-15T11:00:00Z",
+    "comments": "Check email.",
+   
+  },
+  {
+    "startTime": "2023-09-15T15:30:00Z",
+    "endTime": "2023-09-15T17:00:00Z",
+    "comments": "Video call with remote team.",
+    
+  },
+  {
+    "startTime": "2023-09-15T12:30:00Z",
+    "endTime": "2023-09-15T13:30:00Z",
+    "comments": "Lunch with colleagues.",
+   
+  },
+  {
+    "startTime": "2023-09-15T09:30:00Z",
+    "endTime": "2023-09-15T11:30:00Z",
+    "comments": "Meeting with HR.",
+    
+  }
+];
+
+
+
+module.exports = tasks
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const colors = require('colors');
 const morgan = require('morgan');
+const tasks = require('./models/tasks')
 const PORT = process.env.PORT;
 const methodOverride = require('method-override');
 //Intialization....
@@ -12,10 +13,22 @@ app.use(morgan('dev'));
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
 
-//RROUTES..
+//ROUTES..
+
+//Index Route..
+app.get('/todo', (req, res) => {
+    res.render('index.ejs', {
+        allTasks: tasks
+    } )
+})
 
 
-
+//Show Route...
+app.get('/todo/:id', (req, res) => {
+    res.render('show.ejs', {
+        task: tasks[req.params.id]
+    })
+})
 //Listeners
 app.listen(PORT, () => {
     console.log('====================================');
